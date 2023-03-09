cf3d432cf4cee613ea1f0b6c21ee1cc4274703cd
type-donghyun <118829460+type-donghyun@users.noreply.github.com>
2023-03-10 07:40:12 +0900

    Update Office365_install.bat

--- Office365_install.bat
+++ Office365_install.bat
@@ -367,9 +367,7 @@ ECHO   ^<Updates Enabled="TRUE" /^>>>configuration.xml
 ECHO   ^<RemoveMSI /^>>>configuration.xml
 ECHO ^</Configuration^>>>configuration.xml
 
-IF NOT EXIST temp\ (
-	MKDIR temp >nul 2>&1
-)
+MKDIR temp >nul 2>&1
 MOVE /y configuration.xml temp >nul 2>&1
 
 CD TEMP >nul 2>&1
