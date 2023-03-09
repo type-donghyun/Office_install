7b88579e5dc9988a2062c656c6ed457d349f8aef
type-donghyun <118829460+type-donghyun@users.noreply.github.com>
2023-03-09 17:26:38 +0900

    Add files via upload

--- Office365_install.bat
+++ Office365_install.bat
@@ -1,22 +1,24 @@
 @ECHO OFF
 ::================================================================================관리자 권한 요청
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-IF %errorlevel% NEQ 0 (
+
+IF %errorlevel% neq 0 (
 	GOTO UACPrompt
 ) ELSE (
 	GOTO gotAdmin
 )
 
 :UACPrompt
-ECHO Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-ECHO UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
-"%temp%\getadmin.vbs"
-EXIT /B
+	ECHO SET UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+	ECHO UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+	"%temp%\getadmin.vbs"
+	EXIT /B
 
 :gotAdmin
-IF EXIST "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
-PUSHD "%CD%"
-CD /D "%~dp0"
+	IF EXIST "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+	PUSHD "%CD%"
+	CD /D "%~dp0"
 ::====================================================================================================
 
 ::================================================================================ECHO 색상 설정
@@ -359,10 +361,10 @@ ECHO   ^<Updates Enabled="TRUE" /^>>>configuration.xml
 ECHO   ^<RemoveMSI /^>>>configuration.xml
 ECHO ^</Configuration^>>>configuration.xml
 
-MKDIR temp 2>nul
+MKDIR temp 2> nul
 MOVE /y configuration.xml temp 2> nul
 
-CD TEMP 2>nul
+CD TEMP 2> nul
 ECHO 설치 파일 다운로드 중...
 setup.exe /download configuration.xml
 
